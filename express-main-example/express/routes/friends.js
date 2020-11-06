c431884513702937f341a80ce361ec0b065e721b
David Schwartz <dbschwartz2@gmail.com>
2020-11-05 18:52:42 -0600

    express-main-example finalized commit

--- express-main-example/express/routes/friends.js
+++ express-main-example/express/routes/friends.js
@@ -3,13 +3,7 @@ const { Op } = require("sequelize");
 const { getIdParam } = require('../helpers');
 const users = require('./users');
 
-async function getAll(req, res) {
-	const users = await models.user.findAll({
-		attributes: ['id', 'firstName', 'lastName']
-	}
-	);
-	res.status(200).json(users);
-};
+
 
 async function getById(req, res) {
 	const id = getIdParam(req);
@@ -45,7 +39,6 @@ const friendsIds = friends1Ids.concat(friends2Ids)
 			where: {
 				id: friendsIds
 			}
-
 		}
 		);
 		res.status(200).json(friends);
@@ -54,45 +47,8 @@ const friendsIds = friends1Ids.concat(friends2Ids)
 	}
 };
 
-async function create(req, res) {
-	if (req.body.id) {
-		res.status(400).send(`Bad request: ID should not be provided, since it is determined automatically by the database.`)
-	} else {
-		await models.user.create(req.body);
-		res.status(201).end();
-	}
-};
-
-async function update(req, res) {
-	const id = getIdParam(req);
-
-	// We only accept an UPDATE request if the `:id` param matches the body `id`
-	if (req.body.id === id) {
-		await models.user.update(req.body, {
-			where: {
-				id: id
-			}
-		});
-		res.status(200).end();
-	} else {
-		res.status(400).send(`Bad request: param ID (${id}) does not match body ID (${req.body.id}).`);
-	}
-};
-
-async function remove(req, res) {
-	const id = getIdParam(req);
-	await models.user.destroy({
-		where: {
-			id: id
-		}
-	});
-	res.status(200).end();
-};
 
 module.exports = {
-	getAll,
-	getById,
-	create,
-	update,
-	remove,
+	getById
+
 };
